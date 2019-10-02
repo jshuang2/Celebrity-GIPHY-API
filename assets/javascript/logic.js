c1e8aa3a96645d8ea44d734974dd33ed5fd2f87a
Jeff Huang <jeffshuang2@gmail.com>
2019-10-02 14:02:20 -0700

    Added more functions to enable enable api functionality

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,6 +1,16 @@
 //Create an array that will hold the list of celebrities
 var celebrities = ["leonardo dicaprio", "carey mulligan", "tom hardy", "shailene woodley"]
 
+
+function displayCelebGif() {
+    var thisCeleb = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + thisCeleb + "&api_key=GnKyD9C3X7IDAdHB6lUZKXonzOyYEYwK&limit=20"
+
+    
+}
+
+
+
 //Create a function that will render buttons
 function renderButtons() {
     $("#buttons-view").empty();
@@ -15,9 +25,17 @@ function renderButtons() {
     }
 }
 
+//Create a function that will take an event, push a new celebrity to the celebrities array, and create a button for it.
 function addCelebrity() {
     $("#add-celebrity").on("click", function(event) {
         event.preventDefault();
-    })
+
+        var celeb = $("#celebrity-input").val().trim();
+        
+        celebrities.push(celeb);
+        console.log(celebrities);
+
+        renderButtons();
+    });
 }
 renderButtons();
