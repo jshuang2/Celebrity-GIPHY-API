b20248dce0afff9551dd32dd0be1c2f8c3b528fb
Jeff Huang <jeffshuang2@gmail.com>
2019-10-02 14:49:17 -0700

    Gifs now properly load across the screen with a limit of 20 gifs per query

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,12 +1,34 @@
 //Create an array that will hold the list of celebrities
 var celebrities = ["leonardo dicaprio", "carey mulligan", "tom hardy", "shailene woodley"]
 
-
+//Create a function that will add gifs to the html via the API when buttons are clicked
 function displayCelebGif() {
     var thisCeleb = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + thisCeleb + "&api_key=GnKyD9C3X7IDAdHB6lUZKXonzOyYEYwK&limit=20"
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + thisCeleb + "&api_key=GnKyD9C3X7IDAdHB6lUZKXonzOyYEYwK&limit=20";
 
-    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+
+        var results = response.data;
+
+        for(var i = 0; i < results.length; i++) {
+            
+            var gifDiv = $("<div>");
+            var rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+            var gifImage = $("<img>");
+
+            gifImage.attr("src", results[i].images.fixed_height.url);
+
+            gifDiv.append(p);
+            gifDiv.append(gifImage);
+
+            $("#celebrity-view").prepend(gifDiv);
+        }
+    });
 }
 
 
@@ -38,4 +60,7 @@ function addCelebrity() {
         renderButtons();
     });
 }
+
+$(document).on("click", ".celeb-button", displayCelebGif);
+
 renderButtons();
